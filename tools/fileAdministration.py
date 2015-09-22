c216b1ccb77e12928db2a3ea865f106fe91bcbe7
Rasilgon <Rasilgon@users.noreply.github.com>
2015-09-22 12:53:58 +0200

    Create fileAdministration.py

---
+++ tools/fileAdministration.py
@@ -0,0 +1,11 @@
+
+def copy(src, dest):
+#Joey Payne : http://pythoncentral.io/how-to-recursively-copy-a-directory-folder-in-python/
+    try:
+        shutil.copytree(src, dest, ignore=ignore_patterns('*.py', '*.sh', 'specificfile.file'))
+    except OSError as e:
+        # If the error was caused because the source wasn't a directory
+        if e.errno == errno.ENOTDIR:
+            shutil.copy(src, dest)
+        else:
+            print('Directory not copied. Error: %s' % e)
